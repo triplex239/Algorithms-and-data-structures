c6edcaa7fc10932675462bb3bec7d990bba8a1de
3plexGB <triplex239@yandex.ru>
2021-06-29 23:58:31 +0300

    верная версия

---
+++ algorithms_les1/algorithms_les1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms_les1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Number(n);
+        }
+        static void Number (int n)
+        {
+            int d = 0, i = 2;
+            while (i < n)
+            {
+                while (n % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                i++;
+            }
+            if (d == 0) Console.WriteLine("простое");
+            else Console.WriteLine("состовное");
+            Console.ReadLine();
+        }
+    }
+}
